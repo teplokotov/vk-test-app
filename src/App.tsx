1c89ba6417dc6fc100dab0985245b9a00848c9fb
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2024-03-14 22:45:05 +0300

    feat: add fetching age

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { useEffect, useRef, useState } from 'react'
 import styles from './App.module.css'
-import { baseUrl, btnDefaultValue, btnErrorValue, btnLoadingValue } from './utils/constants';
+import { baseUrlAge, baseUrlFact, btnDefaultValue, btnDefaultValueAge, btnErrorValue, btnLoadingValue } from './utils/constants';
 
+let controller: AbortController, signal;
 function App() {
 
   const [fact, setFact] = useState<string>('');
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [hasError, setHasError] = useState<boolean>(false);
   const [btnValue, setBtnValue] = useState<string>(btnDefaultValue);
-  const inputRef = useRef<HTMLInputElement>(null);
+  const inputFactRef = useRef<HTMLInputElement>(null);
+  
+  const [btnValueAge, setBtnValueAge] = useState<string>(btnDefaultValueAge);
+  const inputAgeRef = useRef<HTMLInputElement>(null);
+  const [age, setAge] = useState<string>('');
+  const [username, setUsername] = useState<string>('');
+  const [previousUsername, setPreviousUsername] = useState<string>('');
+  const [isLoadingAge, setIsLoadingAge] = useState<boolean>(false);
+  const [hasErrorAge, setHasErrorAge] = useState<boolean>(false);
+  const btnAgeRef = useRef<HTMLButtonElement>(null);
  
   useEffect(() => {
     moveCursor();
@@ -18,7 +28,7 @@ function App() {
     setIsLoading(true);
     setBtnValue(btnLoadingValue);
     setHasError(false);
-    fetch(baseUrl)
+    fetch(baseUrlFact)
       .then(res => res.json())
       .then(data => {
         setFact(data.fact);
@@ -33,32 +43,87 @@ function App() {
   }
 
   function moveCursor(): void {
-    if (inputRef.current) {
+    if (inputFactRef.current) {
+      inputFactRef.current.selectionStart = 0;
+      inputFactRef.current.selectionEnd = 0;
       const position = fact.split(' ')[0].length
-      inputRef.current.focus();
-      inputRef.current.selectionStart = position;
+      inputFactRef.current.focus();
+      inputFactRef.current.selectionStart = position;
     }
   }
 
-  function handleOnClick(): void {
+  function handleOnClickFact(): void {
     fetchData();
   }
 
+  function handleOnClickAge(): void {
+
+    if(previousUsername === username) return;
+    if (controller) {
+      controller.abort();
+    }
+    controller = new AbortController();
+    signal = controller.signal;
+
+    setIsLoadingAge(true);
+    setBtnValueAge(btnLoadingValue);
+    setHasErrorAge(false);
+    fetch(`${baseUrlAge}?name=${username}`, {
+      signal: signal,
+    })
+      .then(res => res.json())
+      .then(data => {
+        setAge(data.age);
+        setIsLoadingAge(false);
+        setBtnValueAge(btnDefaultValueAge);
+        setPreviousUsername(username);
+      })
+      .catch(err => {
+        if (err.name === 'AbortError') return;
+        console.log(err);
+        setHasErrorAge(true);
+        setBtnValueAge(btnErrorValue);
+      });
+  }
+
+  useEffect(() => {
+    if(username === '' || !username.match(/[a-z]/i)) {
+      btnAgeRef.current?.setAttribute('disabled', '');
+    } else {
+      btnAgeRef.current?.removeAttribute('disabled');
+    }
+  }, [username])
+
   return (
     <main className={styles.main}>
-      <form onSubmit={e => e.preventDefault()} className={styles.form}>
+      <form onSubmit={e => e.preventDefault()} className={styles.formFacts}>
         <input
           className={styles.input}
-          ref={inputRef} 
+          ref={inputFactRef} 
           type='text'
-          defaultValue={!isLoading && !hasError ? fact : ''}
-          placeholder="Нажмите кнопку..."
+          defaultValue={fact}
+          placeholder="Узнайте интересный факт..."
         />
         <button 
           className={styles.button}
-          onClick={handleOnClick}
+          onClick={handleOnClickFact}
         >{btnValue}</button>
       </form>
+      <form onSubmit={e => e.preventDefault()} className={styles.formAge}>
+        <input
+          className={styles.input}
+          ref={inputAgeRef} 
+          type='text'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Введите ваше имя на латинице..."
+        />
+        <button ref={btnAgeRef}
+          className={styles.button}
+          onClick={handleOnClickAge}
+        >{btnValueAge}</button>
+      </form>
+      {!isLoadingAge && !hasErrorAge && age && <p className={styles.age}>Ваш возраст: {age}</p>}
     </main>
   )
 }
