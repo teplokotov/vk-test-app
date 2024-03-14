08573047d7cca7ec18fc5c0d6ceacf1c36f03c1e
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2024-03-14 20:22:42 +0300

    feat: add fetching facts and moving cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from 'react'
+import styles from './App.module.css'
+import { baseUrl, btnDefaultValue, btnErrorValue, btnLoadingValue } from './utils/constants';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [fact, setFact] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [hasError, setHasError] = useState<boolean>(false);
+  const [btnValue, setBtnValue] = useState<string>(btnDefaultValue);
+  const inputRef = useRef<HTMLInputElement>(null);
+ 
+  useEffect(() => {
+    moveCursor();
+  }, [fact]);
+
+  function fetchData(): void {
+    setIsLoading(true);
+    setBtnValue(btnLoadingValue);
+    setHasError(false);
+    fetch(baseUrl)
+      .then(res => res.json())
+      .then(data => {
+        setFact(data.fact);
+        setIsLoading(false);
+        setBtnValue(btnDefaultValue);
+      })
+      .catch(err => {
+        console.log(err);
+        setHasError(true);
+        setBtnValue(btnErrorValue);
+      });
+  }
+
+  function moveCursor(): void {
+    if (inputRef.current) {
+      const position = fact.split(' ')[0].length
+      inputRef.current.focus();
+      inputRef.current.selectionStart = position;
+    }
+  }
+
+  function handleOnClick(): void {
+    fetchData();
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className={styles.main}>
+      <form onSubmit={e => e.preventDefault()} className={styles.form}>
+        <input
+          className={styles.input}
+          ref={inputRef} 
+          type='text'
+          defaultValue={!isLoading && !hasError ? fact : ''}
+          placeholder="Нажмите кнопку..."
+        />
+        <button 
+          className={styles.button}
+          onClick={handleOnClick}
+        >{btnValue}</button>
+      </form>
+    </main>
   )
 }
 
---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+export const baseUrl = 'https://catfact.ninja/fact';
+
+export const btnDefaultValue = 'Получить факт';
+export const btnLoadingValue = '⌛ Загрузка...';
+export const btnErrorValue = '🙁 Ошибка';
