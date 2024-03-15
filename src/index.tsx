5a99e1df0b4f7f3934eae71dca348a87ca8748d2
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2024-03-15 20:29:46 +0300

    feat: add deploying to vk-mini-apps

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './app/app'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app/app';
+import vkBridge from '@vkontakte/vk-bridge';
+import './index.css';
+
+vkBridge.send('VKWebAppInit');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vk-test-app/',
+  // base: '/vk-test-app/', // for gh-pages
+  base: './',               // for vk-mini-apps
   plugins: [react()],
 })
